459ee08e29684a1dd98b8d80957e8df3e0dd6053
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-12-05 16:57:06 +0000

    Added player movement

--- MonthlyChallenge12-20/Program.cs
+++ MonthlyChallenge12-20/Program.cs
@@ -9,14 +9,26 @@ namespace MonthlyChallenge12_20
     class Program
     {
         public static List<TileType> AllTypes = new List<TileType>();
-        public static TileType[,] map = new TileType[10, 10]; 
+        public static TileType[,] map = new TileType[10, 10];
+
+        public static int PlayerX = 5;
+        public static int PlayerY = 5;
         static void Main(string[] args)
         {
             Setup();
+
+            while (true)
+            {
+                Input(Console.ReadKey());
+                Logic();
+                Render();
+            }
         }
 
         public static void Setup()
         {
+            Console.CursorVisible = false;
+
             TileType WallTile = new TileType();
             WallTile.BackgroundColour = ConsoleColor.Gray;
             WallTile.ForegroundColour = ConsoleColor.DarkGray;
@@ -29,7 +41,7 @@ namespace MonthlyChallenge12_20
             GroundTile.BackgroundColour = ConsoleColor.DarkYellow;
             GroundTile.ForegroundColour = ConsoleColor.DarkGreen;
             GroundTile.DisplaySymbol = '.';
-            GroundTile.Passable = false;
+            GroundTile.Passable = true;
             GroundTile.SearchableType = TileTypeEnum.Ground;
             AllTypes.Add(GroundTile);
 
@@ -37,12 +49,19 @@ namespace MonthlyChallenge12_20
             {
                 for (int y = 0; y < map.GetLength(1); y++)
                 {
-                    map[x, y] = AllTypes.Where(i => i.SearchableType == TileTypeEnum.Ground).ToArray()[0];
+                    if (x == 0 || y == 0 || x == map.GetLength(0)-1 || y == map.GetLength(1)-1)
+                    {
+                        map[x, y] = AllTypes.Where(i => i.SearchableType == TileTypeEnum.Wall).ToArray()[0];
+                    }
+                    else
+                    {
+                        map[x, y] = AllTypes.Where(i => i.SearchableType == TileTypeEnum.Ground).ToArray()[0];
+                    }
+                    
                 }
             }
 
             Render();
-            Console.Read();
         }
 
 
@@ -57,6 +76,47 @@ namespace MonthlyChallenge12_20
                     Console.BackgroundColor = map[x, y].BackgroundColour;
                     Console.Write(map[x, y].DisplaySymbol);
                 }
+            }
+
+            Console.SetCursorPosition(PlayerX, PlayerY);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("O");
+        }
+
+        public static void Input(ConsoleKeyInfo Key)
+        {
+            switch (Key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    if (PlayerX > 0 && map[PlayerX-1, PlayerY].Passable)
+                    {
+                        PlayerX--;
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    if (PlayerY > 0 && map[PlayerX, PlayerY - 1].Passable)
+                    {
+                        PlayerY--;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    if (PlayerX < map.GetLength(0) && map[PlayerX + 1, PlayerY].Passable)
+                    {
+                        PlayerX++;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    if (PlayerY < map.GetLength(1) && map[PlayerX, PlayerY+1].Passable)
+                    {
+                        PlayerY++;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
 
